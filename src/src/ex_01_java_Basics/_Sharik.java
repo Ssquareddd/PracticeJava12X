1ae63423c970b68a53c0032fcb3812043b83763e
Ssquareddd <shariksayyed789@gmail.com>
2025-05-12 04:07:54 +0100

    new commit

--- src/src/ex_01_java_Basics/_Sharik.java
+++ src/src/ex_01_java_Basics/_Sharik.java
@@ -2,6 +2,6 @@ package ex_01_java_Basics;
 
 public class _Sharik {
     public static void main(String[] args) {
-
+        System.out.println("This is the file where we can start the class name by _Sharik.java");
     }
 }
