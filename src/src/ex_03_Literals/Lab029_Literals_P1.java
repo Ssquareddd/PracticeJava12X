bf0facc0841935058c34c0602c4d6d52544880bf
Ssquareddd <shariksayyed789@gmail.com>
2025-05-16 04:48:56 +0100

    Third commit

---
+++ src/src/ex_03_Literals/Lab029_Literals_P1.java
@@ -0,0 +1,18 @@
+package ex_03_Literals;
+
+public class Lab029_Literals_P1 {
+    public static void main(String[] args) {
+
+        int age = 65;
+        System.out.println(age);
+        // Literal - Integral
+
+        final float PI = 3.14f;
+        System.out.println(PI);
+        // literal - float
+
+        final int dead_girl = 18;
+        System.out.println(dead_girl);
+        // here i have used literal - Integral have set constant value to it by adding final
+    }
+}
