118fda89910aaa696fecbd50d1cf9e6dddd49b5b
Ssquareddd <shariksayyed789@gmail.com>
2025-06-20 05:53:30 +0100

    commiting 20-06-2025

---
+++ src/src/ex_18_oops_constructors/Car2.java
@@ -0,0 +1,21 @@
+package ex_18_oops_constructors;
+
+public class Car2 {
+    String model;
+    int year;
+
+    Car2(){
+        model = "XXX";
+        year = 1900;
+        System.out.println("DC");
+    }
+
+    Car2(String model_name,int year_created){
+        this("3000");
+        this.model = model_name;
+        this.year = year_created;
+    }
+    Car2(String model_name){
+        this.model = model_name;
+    }
+}
